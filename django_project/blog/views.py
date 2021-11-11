df18ace905804ca2696c859b152f4538df3a38cb
Kellan Anderson <kdanderson5@catamount.wcu.edu>
2021-11-11 16:41:36 -0500

    Finished the third video, Templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# dummy data
+posts = [
+    {
+        'author': 'Kellan Anderson',
+        'title': 'Blog post 1',
+        'content': 'first post content',
+        'date_posted': '11/11/21'
+    },
+    {
+        'author': 'Kellan Anderson',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': '11/10/21'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context=context)
 
 
-def about(Request):
-    return HttpResponse('<h1>Blog About</h1>')
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
