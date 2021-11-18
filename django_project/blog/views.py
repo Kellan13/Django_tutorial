eca396f1f39e50ee24120cd4f5b81e4569dde144
Kellan Anderson <kdanderson5@catamount.wcu.edu>
2021-11-17 20:55:59 -0500

    Finished video 5: Databases

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-# dummy data
-posts = [
-    {
-        'author': 'Kellan Anderson',
-        'title': 'Blog post 1',
-        'content': 'first post content',
-        'date_posted': '11/11/21'
-    },
-    {
-        'author': 'Kellan Anderson',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': '11/10/21'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()  # Pycharm throws error, code still works
     }
     return render(request, 'blog/home.html', context=context)
 
